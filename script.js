3ea8338a6178281487546d3e051bd5613448a022
Valentin-GSO <yuriosorio19@gmail.com>
2024-09-10 13:39:18 -0500

    XD

---
+++ script.js
@@ -0,0 +1,8 @@
+function wrongChoice() {
+    alert("Intenta de nuevo. Este no es el correcto.");
+}
+
+function rightChoice() {
+    // Redirige a la página con la carta
+    window.location.href = "carta.html";
+}
